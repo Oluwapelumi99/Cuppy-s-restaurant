73716928fabd3f10560b4b12a3fce6d4ab49060e
Oluwapelumi Fadare <oluwajosephine7@gmail.com>
2024-09-19 15:20:52 +0000

    Recreate the migrations using zero migration

--- booking/models.py
+++ booking/models.py
@@ -4,11 +4,26 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 
-class Booking(models.Model):
+# class Booking(models.Model):
+#     name = models.CharField(max_length=100)
+#     email = models.EmailField()
+#     phone_number = models.CharField(max_length=11)
+#     number_of_guests = models.IntegerField()
+#     booking_dateandtime_start = models.DateTimeField()
+#     booking_dateandtime_end = models.DateTimeField()
+
+
+class Customer(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
     phone_number = models.CharField(max_length=11)
+
+class Table(models.Model):
+    seats = models.IntegerField()
     number_of_guests = models.IntegerField()
+
+class Booking(models.Model):
     booking_dateandtime_start = models.DateTimeField()
     booking_dateandtime_end = models.DateTimeField()
-    
+
+
--- booking/views.py
+++ booking/views.py
@@ -5,27 +5,29 @@ from django.http import HttpResponse
 from .models import Booking
 
 # Create your views here
+# def booking(request):
+#     return HttpResponse('This is the booking page')
+
 def booking(request):
-    return HttpResponse('This is the booking page')
+    available_tables = Booking.objects.filter(seats__gte=1)
+    return render(request, 'booking.html', {'available_tables': available_tables})
 
-# def ViewBookingTimes(request, date):
-#     available_tables = Table.objects.filter(seats__gte=1)
-#     return render(request, 'booking.html', {'available_tables': available_tables})
 
-# def MakeBooking(request):
-#     if request.method == 'POST':
-#         table_num = int(request.POST['table_num'])
-#         date = request.POST['date']
-#         time = request.POST['time']
-#         people_num = int(request.POST['people_num'])
 
-#         booking = Booking(table_num=table_num, date=date, time=time, people_num=people_num)
-#         # booking.save(commit=False)
-#         # booking.post = post 
-#         # booking.approved = False
-#         booking.save()
-#         messages.add_message(request, messages.SUCCESS, 'Your booking has been completed succesffully!')
-#     else:
-#         messages.add_message(request, messages.ERROR, 'Error making booking, Please send an email to.........')
+def MakeBooking(request):
+    if request.method == 'POST':
+        table_num = int(request.POST['table_num'])
+        date = request.POST['date']
+        time = request.POST['time']
+        people_num = int(request.POST['people_num'])
 
-#     return HttpResponseRedirect(reverse('booking_detail', args=[slug]))
+        booking = Booking(table_num=table_num, date=date, time=time, people_num=people_num)
+        # booking.save(commit=False)
+        # booking.post = post 
+        # booking.approved = False
+        booking.save()
+        messages.add_message(request, messages.SUCCESS, 'Your booking has been completed succesffully!')
+    else:
+        messages.add_message(request, messages.ERROR, 'Error making booking, Please send an email to.........')
+
+    return HttpResponseRedirect(reverse('booking_detail', args=[slug]))
