f1078f1f27a52fd053637e3b184d928c25a14bcb
Oluwapenlumi Fadare <oluwajosephine7@gmail.com>
2024-10-13 20:19:58 +0000

    delete booking.js file

--- home_page/test_views.py
+++ home_page/test_views.py
@@ -6,44 +6,45 @@ from .forms import ReviewForm
 from .models import Review
 from django.urls import reverse
 
-
+USER_MODEL = get_user_model()
 class TestReviewViews(TestCase):
 
-    def setUp(self):
-        """ Set up a staff user and log them in
-        to test the blog crud functionality
-        """
-        username="myUsername",
-        email="test@test.com",
-        password="myPassword"
-        user_model = get_user_model()
-        self.user = user_model.objects.create_superuser(
-            username=username,
-            email=email,
-            password=password,
-        )
-        log_in = self.client.login(password=password,
-            email=email
-            )
 
-        #confirm user is logged in
-        self.assertTrue(log_in)
-        self.assertTrue(self.user.is_staff)
-        # create a review for the database
-        # body="Test review views",
-        # objecrating="4",
-        # self.review = Review.(body="Test review views", author=self.user,
-        #                  rating="4",
-        #                  )
-        # self.review.save()
+    @classmethod
+    def setUpTestData(self):
+        self.user = USER_MODEL.objects.create_user(
+            email='janedoe@test.com',
+            first_name='Jane',
+            last_name='Doe',
+            username='user123',
+            password='password456'
+        )
+
+    
+        self.review = Review.objects.create(
+            body='My review',
+            rating='4',
+            author=self.user,
+        )
 
 
     def test_render_review_page_with_review_form(self):
         response = self.client.get(reverse(
                 'review_list'))
         self.assertEqual(response.status_code, 200)
-        self.assertIn(b"Test review views", response.content)
+        self.assertIn(b"review", response.content)
         self.assertIn(b"4", response.content)
 
+# class TestBlogViews(TestCase):
 
+#     def setUp(self):
+#         self.user = User.objects.create_superuser(
+#             username="myUsername",
+#             password="myPassword",
+#             email="test@test.com"
+#         )
+#         self.post = Post(title="Blog title", author=self.user,
+#                          slug="blog-title", excerpt="Blog excerpt",
+#                          content="Blog content", status=1)
+#         self.post.save()
   
